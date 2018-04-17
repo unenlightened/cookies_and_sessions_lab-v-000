819ed96b73632acdffd0094f680493488152887d
Chely Ho <adept_inept@yahoo.com>
2018-04-17 19:25:30 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,7 +5,7 @@ class ProductsController < ApplicationController
   def add
     @product = params[:product]
     cart << @product
-    redirect_to products_index_path
+    redirect_to root_path
   end
 
 end
