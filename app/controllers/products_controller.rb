fb19174e0f237149c3bd6346d13d830e552d58b9
Chely Ho <adept_inept@yahoo.com>
2018-04-17 18:48:06 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,11 @@
 class ProductsController < ApplicationController
-  def index    
+  def index
+    @cart = Cart.new
   end
 
   def add
-
+    @item = params[:item]
+    cart << @item
   end
 
 end
