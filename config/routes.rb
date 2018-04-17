21edfa884c73cff4a932f5927d8dc9569ed8e9e8
Chely Ho <adept_inept@yahoo.com>
2018-04-17 19:18:36 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -57,5 +57,5 @@ Rails.application.routes.draw do
   root 'products#index'
   resources :product, only: [:index]
 
-  get 'products/add' => 'catalog#add'
+  get 'products/add' => 'products#add'
 end
